c68514cad030c37b8176c30fcc63d2a7d799c5de
Freddy <freddyescobar@hotmail.fr>
2021-02-12 15:33:21 +0100

    feat: handle more errors

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,11 +1,82 @@
 const status = require('http-status');
+const { TokenExpiredError } = require('jsonwebtoken');
+const {
+  ValidationError,
+  NotFoundError,
+  DBError,
+  UniqueViolationError,
+  NotNullViolationError,
+  ForeignKeyViolationError,
+  CheckViolationError,
+  DataError,
+} = require('objection');
 const config = require('../config/config');
 const logger = require('../config/logger');
+const IncorrectCredentialsError = require('../exceptions/IncorrectCredentialsError');
+const JsonWebTokenBlacklistedError = require('../exceptions/JsonWebTokenBlacklistedError');
 const ApiError = require('../utils/ApiError');
 
 const errorConverter = (err, _req, _res, next) => {
   if (err instanceof ApiError) {
     return next(err);
+  }
+
+  if (err instanceof ValidationError) {
+    switch (err.type) {
+      case 'ModelValidation':
+        return next(new ApiError(status.BAD_REQUEST, err.message, true));
+      case 'RelationExpression':
+        return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+      case 'UnallowedRelation':
+        return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+      case 'InvalidGraph':
+        return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+      default:
+        return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+    }
+  }
+
+  if (err instanceof NotFoundError) {
+    return next(new ApiError(status.NOT_FOUND, status[status.NOT_FOUND], true));
+  }
+
+  if (err instanceof UniqueViolationError) {
+    return next(new ApiError(status.CONFILCT, status[status.CONFLICT], false));
+  }
+  if (err instanceof NotNullViolationError) {
+    return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+  }
+
+  if (err instanceof ForeignKeyViolationError) {
+    return next(new ApiError(status.CONFILCT, status[status.CONFLICT], false));
+  }
+
+  if (err instanceof CheckViolationError) {
+    return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+  }
+
+  if (err instanceof DataError) {
+    return next(new ApiError(status.BAD_REQUEST, status[status.BAD_REQUEST], false));
+  }
+
+  if (err instanceof DBError) {
+    return next(new ApiError(
+      status.INTERNAL_SERVER_ERROR,
+      status[status.INTERNAL_SERVER_ERROR],
+      false,
+    ));
+  }
+
+  if (err instanceof IncorrectCredentialsError) {
+    return next(new ApiError(status.UNAUTHORIZED, err.message, true));
+  }
+
+  if (err instanceof JsonWebTokenBlacklistedError) {
+    return next(new ApiError(status.UNAUTHORIZED, err.message, true));
+  }
+
+  if (err instanceof TokenExpiredError) {
+    return next(new ApiError(status.UNAUTHORIZED, err.message, true));
   }
 
   let error = err;
